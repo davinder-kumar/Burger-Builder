21611d328c3a3c251532498aec8d32a5064cfc12
Davinder Kumar <davinderkumar8166@gmail.com>
2020-01-17 00:34:09 +0530

    Add Navigation

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import classes from './Toolbar.module.css'
 import BurgerLogo from '../../Logo/Logo'
+import NavigationItems from '../Navigationitems/Navigationitems'
 const toolbar = () => {
     return (
         <header className={classes.Toolbar}>
             <div>MENU</div>
             <BurgerLogo />
             <nav>
-                ...
+                <NavigationItems />
             </nav>
         </header>
     );
