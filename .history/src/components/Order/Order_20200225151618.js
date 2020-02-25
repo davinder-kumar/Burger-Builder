a98ba8aa4355c9bb7b836252c116b39da8382b0e
Davinder Kumar <davinderkumar8166@gmail.com>
2020-02-25 18:22:51 +0530

    Delete Oders

---
+++ .history/src/components/Order/Order_20200225151618.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import classes from './Order.module.css'
+
+const Order = (props) => {
+    let ings = []
+    for (let i in props.ingredients) {
+        ings.push({
+            name: i,
+            ingAmount: props.ingredients[i]
+        })
+    }
+
+    const ingOutput = ings.map(ing => (
+        <span style={{ 
+            textTransform: 'capitalize', 
+            display: 'inline-block', 
+            margin: '0 8px', 
+            border : '1px solid #ccc',
+            padding : '5px'
+        }}
+         key={ing.name}>{ing.name} : {ing.ingAmount}</span>
+    ))
+
+    return (
+        <div className={classes.Order}>
+            <p>Name : <strong>{props.name}</strong></p>
+            <p>Ingrediants : {ingOutput}</p>
+            <p>Price : <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+            <button onClick={ alert()}>Delete</button>
+        </div>
+    );
+}
+
+export default Order
