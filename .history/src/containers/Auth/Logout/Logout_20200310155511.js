0ed14ce011a3e45095ef2d739909e4c2b29fc1d5
Davinder Kumar <davinderkumar8166@gmail.com>
2020-03-11 00:34:02 +0530

    Finish

---
+++ .history/src/containers/Auth/Logout/Logout_20200310155511.js
@@ -0,0 +1,23 @@
+import React, {  useEffect } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import * as actionsList from '../../../redux-store/actions'
+import routes from '../../../routes'
+const Logout = props => {
+    const {logoutUser} = props
+    useEffect(()=>{
+        logoutUser()
+    },[logoutUser])
+    
+        return (
+            <Redirect to={routes.home} />
+        )
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        logoutUser: () => dispatch(actionsList.logoutUser())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Logout)
